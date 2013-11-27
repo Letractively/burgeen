d3123b45c18e24cefff1fb814ac13417e8baa6c7
hendranatasaputra@gmail.com <hendranatasaputra@gmail.com@6244d0e7-0429-88c5-ae10-8f0c16469143>
2013-11-27 12:23:04 +0000

    reset password feature

---
+++ trunk/reset_process.php
@@ -0,0 +1,28 @@
+<?php
+			include ("database.php");
+			session_start();
+			
+			$user_id = uniqid('U');
+			$enrypt=md5($_POST[old_password]);
+			$q_reset= "select count(*) from user where password='$enrypt' and username='$_SESSION[username]'";
+			$hq_reset	= mysql_db_query($DataBase,$q_reset);
+			while(list($temp) = mysql_fetch_row($hq_reset))
+			{
+				
+			if($temp == 1)
+			{
+				$enrypt=md5($_POST[new_password_1]);
+				$q_update= "update user set password='$enrypt' where username='$_SESSION[username]'";
+				$hq_update	= mysql_db_query($DataBase,$q_update);
+				
+				header('location:http://127.0.0.1/wordpress2/');
+			}else //error
+			{
+				$_SESSION[reset_password]='failed';
+			   header('location:http://127.0.0.1/wordpress2/reset-password/');
+			}
+			}
+			
+			
+			
+			?>
